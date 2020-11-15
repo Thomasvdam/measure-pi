6ed88a918da492eca045489f90a1e529f551fe92
Thomas van Dam <t.vandam@wearetriple.com>
2020-11-15 22:17:30 +0100

    feat: Add reset indicator to pule callback

--- pulse_tracker.py
+++ pulse_tracker.py
@@ -6,23 +6,23 @@ class PulseTracker:
         self._divisor = divisor
         self._observers = []
 
-    def reset_pulse(self):
-        self._pulse_count = 0
-        self._on_pulse()
-
     def trigger_pulse(self):
         self._pulse_count += 1
-        self._on_pulse()
+        self._on_pulse(False)
 
-    def _on_pulse(self):
+    def reset_pulse(self):
+        self._pulse_count = 0
+        self._on_pulse(True)
+
+    def _on_pulse(self, reset):
         on_divisor = self._pulse_count % self._divisor == 0
         if on_divisor:
             count = math.floor(self._pulse_count / self._divisor)
             for observer in self._observers:
-                observer(count)
+                observer(count, reset)
 
-    def on_division(self, observer):
+    def on_divisor(self, observer):
         self._observers.append(observer)
 
-    def off_division(self, observer):
+    def off_divisor(self, observer):
         self._observers.remove(observer)
